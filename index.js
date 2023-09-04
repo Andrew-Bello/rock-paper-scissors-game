9d8bb0f37fdc374d6110c72c64dafa0b2236cdbb
Andrew Bello <92801912+Andrew-Bello@users.noreply.github.com>
2023-09-04 22:45:57 +0100

    Computer choice function

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Rock paper scissors game. Needs to take two inputs, one from the computer and one from the user.
     // Create a function that takes the computer's input
     // Computer input has to be completely random
+    // Create player input function 
+    // Create a function that does one round of RPS
 
 // Rock > Scissors | Scissors > Paper | Paper > Rock | Everything else is a draw
 
@@ -13,17 +15,37 @@ function getComputerChoice() {
     
     switch (randomInput) {
         case 1:
-           console.log('Rock');
+           randomInput = 'rock';
            break; 
         case 2: 
-            console.log('Paper')
+            randomInput = 'paper'
             break;
         case 3: 
-            console.log('Scissors')
+            randomInput = 'scissors'
             break;    
+    }
+    console.log(randomInput);
+}
+
+function getPlayerChoice() {
+    let playerChoice = prompt('Let\'s play Rock, Paper, Scissors').toLowerCase()
+    if (playerChoice !== 'rock' && 'paper' && 'scissors') {
+        playerChoice = prompt('Please enter valid choice')
+    }
+    return playerChoice;
+}
+
+const playerSelection = getPlayerChoice();
+const computerSelection = getComputerChoice();
+
+function playRound(player, computer) {
+    if (player === 'rock' && computer === 'scissors') {
+        console.log('You win this round')
+    } else if (player === 'paper' && computer === 'rock') {
+        console.log('You win this round')
+    } else if (player === 'scissors' && computer === 'paper') {
+        console.log('You win this round')
     }
 }
 
-
-
-getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
